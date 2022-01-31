d46c2a0b5bf80d74b7b7c2891a2f761238214773
loukikraina <loukik2001@gmail.com>
2022-01-31 15:28:12 +0530

    speech recognition model built

---
+++ laplacian.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  1 00:56:11 2021
+
+@author: LOUKIK RAINA
+"""
+
+from imutils import paths
+import argparse
+import cv2
+def variance_of_laplacian(image):
+	# compute the Laplacian of the image and then return the focus
+	# measure, which is simply the variance of the Laplacian
+	return cv2.Laplacian(image, cv2.CV_64F).var()
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--images", required=True,
+	help="path to input directory of images")
+ap.add_argument("-t", "--threshold", type=float, default=100.0,
+	help="focus measures that fall below this value will be considered 'blurry'")
+args = vars(ap.parse_args())
+
+for imagePath in paths.list_images(args["images"]):
+	# load the image, convert it to grayscale, and compute the
+	# focus measure of the image using the Variance of Laplacian
+	# method
+	image = cv2.imread(imagePath)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	fm = variance_of_laplacian(gray)
+    
+	print(gray,fm)
+	# if the focus measure is less than the supplied threshold,
+	# then the image should be considered "blurry"
+	if fm < args["threshold"]:
+		text = "Blurry"
+	# show the image
+	cv2.putText(image, "{}: {:.2f}".format('not blurry', fm), (10, 30),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+	cv2.imshow("Image", image)
+	key = cv2.waitKey(0)
